3e1e856864a9089416ebe776b3031492eb114a40
fala45 <falla1234hd@hotmail.com>
2025-02-13 12:32:10 -0500

    Alerta que avisa que no se ha registrado un amigo

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let almacenAmigos = [];
+let Amigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    if(nombre.trim() == ''){
+        alert('Por favor agregue un amigo secreto para continuar')
+        return;
+    }
+
+    amigos.push(nombre);
+    console.log(amigos);
+    document.getElementById('amigo').value='';
+
+    mostrarAmigoEnLista();
+}
 
