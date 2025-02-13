9b1b2c3f0fe4eee55bb86aec70ca8ca0d0725d73
fala45 <falla1234hd@hotmail.com>
2025-02-13 17:46:49 -0500

    Agregamos funcion de mostrar lista

--- app.js
+++ app.js
@@ -4,14 +4,31 @@ let Amigos = [];
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
     if(nombre.trim() == ''){
-        alert('Por favor agregue un amigo secreto para continuar')
-        return;
+        alert('Por favor agregue un amigo secreto para continuar');
+    } else{
+        Amigos.push(nombre);
+        document.querySelector('#amigo').value="";
+        mostrarLista();
+       
     }
 
-    amigos.push(nombre);
-    console.log(amigos);
-    document.getElementById('amigo').value='';
+    console.log(nombre);
 
-    mostrarAmigoEnLista();
 }
+
+function mostrarLista(){
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = "";
+    for(let index=0; index < Amigos.length; index++){
+        const element = Amigos[index];
+        let listaHTML = document.createElement('li');
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML);
+        console.log(listaHTML);
+    }
+}
+
+
+
+
 
